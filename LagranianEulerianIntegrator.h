e180e65be1b14e69e70a9c287ff3f92341256ce3
David Beckingsale <davidbeckingsale@gmail.com>
2012-06-13 14:28:40 +0100

    Added LagrangianEulerianIntegrator header.
    The LagrangianEulerianIntegrator will step the patch hierarchy through
    a fully Eulerian step-remap scheme.

---
+++ LagranianEulerianIntegrator.h
@@ -0,0 +1,13 @@
+#ifndef included_LagrangianEulerianIntegrator
+#define included_LagrangianEulerianIntegrator
+
+#include "SAMRAI/algs/TimeRefinementLevelStrategy.h"
+
+using namespace SAMRAI;
+
+class LagrangianEulerianIntegrator:
+    public algs::TimeRefinementLevelStrategy
+{
+};
+
+#endif
